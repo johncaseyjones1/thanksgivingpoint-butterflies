742e280c26051b5d6f2973cfaf55343bb16bbe82
Casey Jones <jcaseyj.5@gmail.com>
2021-02-17 23:05:26 -0700

    Added ObservationDAO with date, time and photoPath

---
+++ src/server/data_access/request/observation/getObservationRequest.py
@@ -0,0 +1,12 @@
+
+
+class GetObservationRequest:
+    def __init__(self, ID, getAll):
+        self.ID = ID
+        self.getAll = getAll
+
+    def getID(self):
+        return self.ID
+
+    def getGetAll(self):
+        return self.getAll
