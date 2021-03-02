b254d5030de6f92ef4768fe27da4340628232a75
Emme Tuft <emmetuft@gmail.com>
2021-03-02 16:03:05 -0700

    Moved photos folder

--- src/server/service/ObservationService.py
+++ src/server/service/ObservationService.py
@@ -5,7 +5,7 @@ parent_dir = os.getcwd() # find the path to module a
 path = os.path.dirname(parent_dir)
 # Add the parent to sys.pah
 sys.path.insert(1, "/home/caseyjones/github/thanksgivingpoint-butterflies/src/server/data_access/")
-from ObservationDAO import ObservationDAO
+from data_access.ObservationDAO import ObservationDAO
 
 from datetime import datetime
 
