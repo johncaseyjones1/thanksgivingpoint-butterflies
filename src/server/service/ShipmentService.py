773915539285dbb75c86f2155c2adb6746ef6343
Emme Tuft <emmetuft@gmail.com>
2021-03-01 17:34:50 -0700

    Created ShipmentService class

---
+++ src/server/service/ShipmentService.py
@@ -0,0 +1,16 @@
+
+import sys, os
+parent_dir = os.getcwd() # find the path to module a
+# Then go up one level to the common parent directory
+path = os.path.dirname(parent_dir)
+# Add the parent to sys.pah
+sys.path.insert(1, "/home/caseyjones/github/thanksgivingpoint-butterflies/src/server/data_access/")
+from ShipmentDAO import ShipmentDAO
+
+
+class InsertShipment:
+
+    @staticmethod
+    def insertOneShipment(request):
+        shipmentDAO = ShipmentDAO()
+        return shipmentDAO.insertOneShipment(request)
