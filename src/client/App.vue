34f8f7bbf4700e69bc175e951b218521e5f87b10
Emme Tuft <emmetuft@gmail.com>
2021-03-01 21:37:01 -0700

    Adding more site-wide styles

--- src/client/App.vue
+++ src/client/App.vue
@@ -14,8 +14,8 @@
   </div> -->
 
   <div id="app">
-        <Navbar />
-</div>
+    <Navbar />
+  </div>
 </template>
 
 <script>
@@ -36,14 +36,33 @@ export default {
 <style>
   /* This style code will affect EVERY page on the website. Please don't change unless you're sure you want to*/
 
+  * {
+    font-family: 'Montserrat', sans-serif;
+  }
+
   h1 {
     font-size: 5vh;
   }
 
-  body {
-    font-size: 2vh;
+  p {
+    font-size: 14px;
   }
 
+  .navbar-light {
+    background-color: white !important;
+  }
+
+  .active {
+    color: #fe4600 !important;
+  }
+
+  .shadow-box {
+    box-shadow: 3px 3px 6px lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    background-color: white;
+    margin-bottom: 30px;
+  }
 
 
 
