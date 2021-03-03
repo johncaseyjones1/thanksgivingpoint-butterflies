a12d10aa3be20b9d73fa96f922dbad6b3455a4e3
Casey Jones <jcaseyj.5@gmail.com>
2021-03-03 14:08:31 -0700

    Added image paths to butterflySecies collection and fixed typos in image names

--- src/server/database_loaders/ButterflySpeciesLoader.py
+++ src/server/database_loaders/ButterflySpeciesLoader.py
@@ -1,11 +1,18 @@
-''' import pandas
-
-excel_data = pandas.read_excel('/home/caseyjones/github/thanksgivingpoint-butterflies/src/server/database_loaders/raw_data/ButterflyMatrix.xlsx',sheet_name="Sheet1")
-
-json_data = excel_data.to_json()
-
-print("Json data:\n{}".format(json_data))
- '''
 from excel2json import convert_from_file
+import json
+import pymongo
 
+# Change to the absolute path of this file on your machine
 convert_from_file('/home/caseyjones/github/thanksgivingpoint-butterflies/src/server/database_loaders/raw_data/ButterflyMatrix.xls')
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["observatory"]
+col = db["butterfly_species"]
+
+# Clear database to prevent duplicates
+col.delete_many({})
+
+with open('/home/caseyjones/github/thanksgivingpoint-butterflies/src/server/database_loaders/raw_data/ButterflySpecies.json') as file: 
+    file_data = json.load(file) 
+
+col.insert_many(file_data)
