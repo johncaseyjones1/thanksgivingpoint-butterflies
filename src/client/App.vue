70bc1ea2bf4c210a308ad597408a3bc3aa55ba49
Emme Tuft <emmetuft@gmail.com>
2021-03-03 10:19:39 -0700

    Removed commented out code

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,18 +1,4 @@
 <template>
-  <!-- <div class="app full-height">
-    <div id="top_bar">
-      <h1>{{ message }}</h1>
-      <div id="router">
-        <router-link class="router_item" :to="{ name: 'Dashboard' }">Dashboard</router-link>
-        <router-link class="router_item" :to="{ name: 'Activities' }">Activities</router-link>
-        <router-link class="router_item" :to="{ name: 'Gallery' }">Gallery</router-link>
-        <router-link class="router_item" :to="{ name: 'ObservationInput' }">Observations</router-link>
-      </div>
-    </div> 
-
-    <router-view />
-  </div> -->
-
   <div id="app">
     <Navbar />
   </div>
