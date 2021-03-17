037982c8f69989532d0b57e16deb9d666c996ddc
Emme Tuft <emmetuft@gmail.com>
2021-03-17 11:34:00 -0600

    Added Vue table and "Add Shipment" button to Shipments component

---
+++ src/client/components/AddShipment.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="add-shipment-container">
+    <div class="center-div">
+      <div>
+        Date:
+      </div>
+      <div>
+        Supplier:
+      </div>
+      <div>
+        Species:
+      </div>
+      <div>
+        Origin:
+      </div>
+      <div>
+        Quantity:
+      </div>
+      <div>
+        Emerged early:
+      </div>
+      <div>
+        Dead on arrival:
+      </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+
+export default {
+  name: 'AddShipment',
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  }
+}
+
+</script>
+
+<style scoped>
+.add-shipment-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px 0 5px;
+  width: 100%;
+}
+.center-div {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  background-color: #eeeeed;
+  border: 3px solid #eeeeed;
+  border-radius: 10px;
+  width: 80%;
+}
+</style>
--- src/client/components/Shipments.vue
+++ src/client/components/Shipments.vue
@@ -1,20 +1,55 @@
 <template>
   <div class="shipment-container">
     <div class="center-div">
-      <v-client-table v-model="shipments" :columns="columns" :options="options">
-        <input type="number" slot="FTE" slot-scope="{row, update}" v-model="row.FTE" @input="update">
+      <button class="btn btn-dark add-shipment-btn" v-show="showTable" @click="showAddShipment()">Add new shipment</button>
+      <button class="btn btn-dark add-shipment-btn" v-show="addShipment" @click="hideAddShipment()">Cancel</button>
+      <v-client-table v-show="showTable" v-model="shipments" :columns="columns" :options="options">
+        <div slot="FTE" slot-scope="{row, update, setEditing, isEditing, revertValue}">
+          <span @click="setEditing(true)" v-if="!isEditing()">
+              {{row.FTE}}
+          </span>
+          <span v-else>
+            <input type="text" class="form-control" v-model="row.FTE">
+            <button type="button" class="btn btn-dark btn-sm" @click="update(row.FTE); setEditing(false)">Submit</button>
+            <button type="button" class="btn btn-sm" @click="revertValue(); setEditing(false)">Cancel</button>        
+          </span>  
+        </div>
+        <div slot="W" slot-scope="{row, update, setEditing, isEditing, revertValue}">
+          <span @click="setEditing(true)" v-if="!isEditing()">
+              {{row.W}}
+          </span>
+          <span v-else>
+            <input type="text" class="form-control" v-model="row.W">
+            <button type="button" class="btn btn-dark btn-sm" @click="update(row.W); setEditing(false)">Submit</button>
+            <button type="button" class="btn btn-sm" @click="revertValue(); setEditing(false)">Cancel</button>        
+          </span>  
+        </div>
+        <div slot="Parasite" slot-scope="{row, update, setEditing, isEditing, revertValue}">
+          <span @click="setEditing(true)" v-if="!isEditing()">
+              {{row.Parasite}}
+          </span>
+          <span v-else>
+            <input type="text" class="form-control" v-model="row.Parasite">
+            <button type="button" class="btn btn-dark btn-sm" @click="update(row.Parasite); setEditing(false)">Submit</button>
+            <button type="button" class="btn btn-sm" @click="revertValue(); setEditing(false)">Cancel</button>        
+          </span>  
+        </div> 
       </v-client-table>
     </div>
+    <AddShipment v-show="addShipment"/>
   </div>
 </template>
 
 <script>
 import request from 'superagent-bluebird-promise'
+import AddShipment from './AddShipment'
 
 export default {
   name: 'Shipments',
   data() {
     return {
+      addShipment: false,
+      showTable: true,
       collectingData: false,
       shipments: [],
       columns: ["id","formattedDate","Supplier","Species","Origin","Quantity","EmergedEarly","DOA","FTE","W","Parasite"],
@@ -32,9 +67,26 @@ export default {
           W: 'Wings',
           Parasite: 'Parasite'
         },
-        editableColumns:['FTE','Parasite']
+        editableColumns:['FTE','W','Parasite'],
+        customSorting: {
+          formattedDate: function (ascending) {
+            return function (a, b) {
+              var dateA = a.formattedDate;
+              var dateB = b.formattedDate;
+
+              if (ascending)
+                return dateA >= dateB ? 1 : -1;
+
+              return dateA <= dateB ? 1 : -1;
+            }
+          }
+        }
       }
     }
+  },
+
+  components: {
+    AddShipment
   },
 
   methods: {
@@ -46,10 +98,18 @@ export default {
           this.shipments = JSON.parse(res.body.allShipments)
           var ind
           for (ind in this.shipments) {
-            this.shipments[ind].formattedDate = new Date(this.shipments[ind].Date.$date).toISOString()
+            this.shipments[ind].formattedDate = new Date(this.shipments[ind].Date.$date).toDateString()
             this.shipments[ind].id = this.shipments[ind]._id.$oid
           }
         })
+    },
+    showAddShipment() {
+      this.addShipment = true;
+      this.showTable = false;
+    },
+    hideAddShipment() {
+      this.addShipment = false;
+      this.showTable = true;
     }
   },
 
@@ -63,6 +123,7 @@ export default {
 <style scoped>
 .shipment-container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   margin: 0 5px 0 5px;
@@ -72,5 +133,8 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
+}
+.add-shipment-btn {
+  margin-bottom: 30px;
 }
 </style>
